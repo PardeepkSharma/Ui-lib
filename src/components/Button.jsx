184e6a13f559853548595fb49eb3e1847435b637
pardeep sharma <parthkumar500@gmail.com>
2025-07-27 19:20:47 +0530

    3nd commit

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 // Button.tsx
-const Button = ({ children, className = "", ...props }) => {
+const Button = ({ children, className2 = "red", className = "", ...props }) => {
   return (
     <button
-      className={`px-4 py-2 bg-blue-500 text-white rounded ${className}`}
+      className={`px-4 py-2 bg-${className2}-500 text-white rounded ${className}`}
       {...props}
     >
       {children}
