051ffa621ca8640ecc2665fd112c0665b3349e3e
pardeep sharma <parthkumar500@gmail.com>
2025-07-29 12:28:54 +0530

    4nd commit

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,4 @@
 // Button.tsx
-const Button = ({ children, className2 = "red", className = "", ...props }) => {
-  return (
-    <button
-      className={`px-4 py-2 bg-${className2}-500 text-white rounded ${className}`}
-      {...props}
-    >
-      {children}
-    </button>
-  );
-};
-
-export default Button;
+export default function Button() {
+  return <button className="bg-blue-600 text-white px-4 py-2">Click Me</button>;
+}
